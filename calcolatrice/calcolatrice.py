0120078dc492b10656aea684620b1a662f8a8dc1
G-E-M <piedigrottalorenzo@gmail.com>
2020-11-11 13:21:04 +0100

    Update calcolatrice.py

--- calcolatrice/calcolatrice.py
+++ calcolatrice/calcolatrice.py
@@ -1,10 +1,42 @@
+import calcolatrice
+from calcolatrice import Animale  
 
-def addizione (a,b):
-    return a+b
+print(calcolatrice.addizione(12,34))
+################################################
+#Variabile
 
-class Animale:
-    def __init__(self,nome) :
-        self.nome = nome
-    def stampa_animale(self):
-        print(self.nome) 
+cane = "pippo"
 
+#Costruttore
+
+cane2= calcolatrice.Animale(cane)
+
+#Istanza
+
+cane2.stampa_animale()
+
+################################################
+
+cane3nome= "apppoiskj"
+cane3 = Animale(cane3nome)
+cane3.stampa_animale()
+
+################################################
+
+#riasegnamento della variabile cane
+
+cane = "Andromeda"
+caneA = Animale(cane)
+caneA.stampa_animale()
+
+################################################
+
+#Nella istanza caneA vado a riassegnare l'attributo nome (presente dove ho definito la classe) 
+caneA.nome= "zippo"
+#Richiamando stampa_animale() stampera anche zippo
+caneA.stampa_animale()
+
+################################################
+
+
+################################################
