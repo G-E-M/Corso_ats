8d62a491be424ae1002ae454cb4f56f390e8e5e6
G-E-M <piedigrottalorenzo@gmail.com>
2020-11-13 14:59:14 +0100

    varie2

---
+++ Connesione_db/connesioni/stabilisco_la_connessione.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+class connessione:
+    def connessione():
+        config = {
+        'user': 'root',
+        'password': '',
+        'host': '127.0.0.1',
+        'database': 'cors_protom',
+        'autocommit':False
+        }
+        
